eb31d1478ab0d8fcd7aecf71ecda82b189fc4a4c
Jacobo Renato Cohello Revolledo <j-acobo@hotmail.com>
2023-04-02 19:36:26 -0500

    Resizing Background, NavBar

--- client/src/Components/NavBar/NavBar.jsx
+++ client/src/Components/NavBar/NavBar.jsx
@@ -21,7 +21,7 @@ export default function NavBar() {
     }, []);
 
     useEffect(() => {
-      setFilterLabel(" " + selectedTemperaments.join(", "));
+      setFilterLabel(" " + selectedTemperaments.filter(Boolean).join(", "));
     }, [selectedTemperaments]);
 
     //---------------------------------------------------------------
